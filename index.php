15cb1292c07ea22bf19ad0c018375cdfef28acdd
Chirag <chiragjn807@gmail.com>
2022-06-10 17:37:43 +0530

    changes

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<?php
+$insert = false;
+if(isset($_POST['name'])){
+    // Set connection variables
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "gym";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password, $database);
+
+    // Check for connection success
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    // echo "Success connecting to the db";
+
+    // Collect post variables
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $sql = "INSERT INTO `gym`.`gym_data` (`name`, `age`, `gender`, `email`, `phone`) VALUES ('$name', '$age', '$gender', '$email', '$phone');";
+    // echo $sql;
+
+    // Execute the query
+    if($con->query($sql) == true){
+        // echo "Successfully inserted";
+
+        // Flag for successful insertion
+        $insert = true;
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+
+    // Close the database connection
+    $con->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
